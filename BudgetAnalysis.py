011912ecfec965ca92550ab8857b98a068e633f5
brent.barbachem <brent.barbachem@sisinc.org>
2019-05-05 19:13:52 -0400

    Fixed up the add and minus button functions. Still some work to be done. To be continued ...

--- BudgetAnalysis.py
+++ BudgetAnalysis.py
@@ -1,7 +1,7 @@
 # NOTE: Python3.6 will be used for PyQt5.
 #!/usr/bin/python3
 
-from PyQt5.QtWidgets import QMainWindow, QApplication, QTableWidget, QTableWidgetItem, QWidget, QTabWidget, QSizePolicy, QHBoxLayout
+from PyQt5.QtWidgets import QMainWindow, QApplication, QTableWidget, QTableWidgetItem, QWidget, QTabWidget, QSizePolicy, QHBoxLayout, QPushButton
 from PyQt5.QtCore import pyqtSlot, QObject, Qt
 import sys
 import qdarkstyle
@@ -22,27 +22,35 @@ class TableItem:
 
 
 class SectionWidget(QWidget):
-    """
-
-    """
 
     def __init__(self, defaults: []) -> None:
         """
-
+        This class will contain the layout for each of the customizeable tabs
+        The tabs will be sections that detail an area of personal finance. The
+        user can change the values by adding their own too.
         """
         super().__init__()
 
         layout = QHBoxLayout()
 
         self._table = QTableWidget()
+        """ Only allow title and amount columns for now """
         self._table.setColumnCount(2)
+        """ Expand to fit the layout """
         self._table.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         self._table.itemChanged.connect(self._item_changed)
+        self._table.itemSelectionChanged.connect(self._save_selection)
 
         layout.addWidget(self._table)
 
+        """ default values where only the amount can change """
         self._defaults = defaults
+        """ user entered custom values for this section """
+        self._custom = []
+        """ list of selected items """
+        self._selected = []
 
+        """ Create the table and fill in all of the data including the custom and default values """
         self._set_table()
 
         self.setLayout(layout)
@@ -54,11 +62,24 @@ class SectionWidget(QWidget):
         :param item: Table Widget Item that was changed
         :return: None
         """
+        converted = 0.0
+
         if item.column() == 1:
             try:
                 converted = float(item.text())
             except ValueError:
                 item.setText(str(0.0))
+
+        if item.row() < len(self._defaults):
+            if item.column() == 0:
+                self._defaults[item.row()].name = item.text()
+            elif item.column() == 1:
+                self._defaults[item.row()].amount = converted
+        else:
+            if item.column() == 0:
+                self._custom[item.row() - len(self._defaults)].name = item.text()
+            elif item.column() == 1:
+                self._custom[item.row() - len(self._defaults)].amount = converted
 
     def _set_table(self) -> None:
         """
@@ -67,20 +88,32 @@ class SectionWidget(QWidget):
         :return: None
         """
         self._table.setRowCount(0)
-        self._table.setRowCount(len(self._defaults))
 
         for i in range(len(self._defaults)):
+            self._table.setRowCount(self._table.rowCount() + 1)
+
             title_item = QTableWidgetItem(self._defaults[i].name)
             title_item.setFlags(title_item.flags() & ~Qt.ItemIsEditable);
-            self._table.setItem(i, 0, title_item)
+            self._table.setItem(self._table.rowCount() - 1, 0, title_item)
 
             amount_item = QTableWidgetItem(str(self._defaults[i].amount))
             amount_item.setTextAlignment(Qt.AlignRight)
-            self._table.setItem(i, 1, amount_item)
+            self._table.setItem(self._table.rowCount() - 1, 1, amount_item)
+
+        for i in range(len(self._custom)):
+            self._table.setRowCount(self._table.rowCount() + 1)
+
+            title_item = QTableWidgetItem(self._custom[i].name)
+            self._table.setItem(self._table.rowCount() - 1, 0, title_item)
+
+            amount_item = QTableWidgetItem(str(self._custom[i].amount))
+            amount_item.setTextAlignment(Qt.AlignRight)
+            self._table.setItem(self._table.rowCount() - 1, 1, amount_item)
 
         self._table.move(0,0)
         self._table.resizeColumnsToContents()
         self._resize_columns()
+        self._add_buttons()
 
     def _resize_columns(self) -> None:
         """
@@ -95,6 +128,75 @@ class SectionWidget(QWidget):
 
         for i in range(self._table.columnCount()):
             self._table.setColumnWidth(i, max_column_width)
+
+    def _add_buttons(self) -> None:
+        """
+        Add in the plus and minus buttons so that the user can add and remove items as they wish.
+        :return: None
+        """
+        self._table.setRowCount(self._table.rowCount() + 1)
+
+        plus = QPushButton()
+        plus.setText("+")
+        plus.clicked.connect(self._add_entry)
+        self._table.setCellWidget(self._table.rowCount() - 1, 0, plus)
+
+        minus = QPushButton()
+        minus.setText("-")
+        minus.clicked.connect(self._remove_entry)
+        self._table.setCellWidget(self._table.rowCount() - 1, 1, minus)
+
+    @pyqtSlot()
+    def _add_entry(self) -> None:
+        """
+        Add a new entry to the custom values so that the user can add custom fields that they can also remove
+        :return: None
+        """
+        self._custom.append(TableItem(""))
+        self._set_table()
+
+    @pyqtSlot()
+    def _remove_entry(self) -> None:
+        """
+        Attempt to remove the item(s) from the list. Note: We should ONLY be able to remove the
+        items that we have added (defaults can not be removed).
+        :return: None
+        """
+        self._selected.sort(reverse=True)
+
+        for row in self._selected:
+            for x in self._custom:
+                if self._table.item(row, 0) and x.name == self._table.item(row, 0).text():
+                    self._custom.remove(x)
+
+            self._table.removeRow(row)
+
+        self._selected = []
+
+    @pyqtSlot()
+    def _save_selection(self) -> None:
+        """
+        We cannot press the minus button without changing the current selection, so we need to save
+        the previous valid selections. We will not save any row that was in the defaults range. This
+        way the user can delete custom entries when they later press the - button
+        :return: None
+        """
+        indexes = self._table.selectedIndexes()
+
+        temp_indexes = []
+
+        """ 
+        Make sure the index is NOT the final row (containing the buttons) 
+        It is NOT alreadyy in the list
+        It is NOT a default
+        """
+        for index in indexes:
+            if len(self._defaults) <= index.row() < self._table.rowCount()-1 and index.row() not in temp_indexes:
+                temp_indexes.append(index.row())
+            elif index.row() == self._table.rowCount() - 1:
+                return
+
+        self._selected = temp_indexes
 
 
 class MainWindow(QMainWindow):
